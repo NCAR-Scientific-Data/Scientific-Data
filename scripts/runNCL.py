31a34b83a4c04a7fed8c792ea7153a845ba8ffee
root <root@vetsscidata.ucar.edu>
2015-01-15 17:39:38 -0700

    runNCL now runs subset, aggregate, and plot

--- scripts/runNCL.py
+++ scripts/runNCL.py
@@ -1,8 +1,18 @@
 import subprocess
+import sys
 
 def runNCL():
-    status = subprocess.call(["ncl", "narccap_subset_tmin_time_latlon.ncl"])
+    status = subprocess.call(["ncl narccap_subset_tmin_time_latlon.ncl"], shell=True)
     if status < 0:
-        print "Error running ncl"
+        print "Error subsetting data"
     else:
-        print "Success"
+      status2 = subprocess.call(["ncl 'infile=\"tmin_subset_time_latlon.nc\"' 'outfile=\"tmin_aggregate_monthly.nc\"' 'varname=\"tmin\"' 'interval=\"month\"' aggregate.ncl"], shell=True)
+      if status2 < 0:
+	print "Error aggregating data"
+      else:
+	status3 = subprocess.call(["ncl 'filename=\"tmin_aggregate_monthly.nc\"' narccap_plot_tmin_latlon.ncl"], shell=True)
+	if status3 < 0:
+	   print "Error plotting data"
+
+if __name__ == "__main__":
+    runNCL()
