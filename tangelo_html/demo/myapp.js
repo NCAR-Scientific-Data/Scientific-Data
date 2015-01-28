c67d8c17aabcb24a4247a54eb6c1ca08909e80d7
Hannah <imaginationandtech@econ1-158-54-dhcp.int.colorado.edu>
2015-01-28 16:59:14 -0700

    Updated with correct corners for lat/long

--- tangelo_html/demo/myapp.js
+++ tangelo_html/demo/myapp.js
@@ -1,5 +1,5 @@
 function runScript() {
-        $.getJSON("runNCL?selat=" + encodeURIComponent(seLat) + "?selon=" + encodeURIComponent(seLon) + "?nwlat=" + encodeURIComponent("nwLat") + "?nwlon=" + encodeURIComponent("nwLon") + "?startyear=" + encodeURIComponent(start) + "?endyear=" + encodeURIComponent(end), function (data) {
+        $.getJSON("runNCL?swlat=" + encodeURIComponent(swLat) + "?swlon=" + encodeURIComponent(swLon) + "?nelat=" + encodeURIComponent("neLat") + "?nelon=" + encodeURIComponent("neLon") + "?startyear=" + encodeURIComponent(start) + "?endyear=" + encodeURIComponent(end), function (data) {
 		if(data.image)
 		{
 			localStorage.setItem("sourceimage?", data.image);
