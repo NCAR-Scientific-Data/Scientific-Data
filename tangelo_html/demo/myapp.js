8aba233eb24b55658ab245f363ca5d2c706a66db
Hannah <imaginationandtech@econ1-158-54-dhcp.int.colorado.edu>
2015-01-28 16:49:18 -0700

    Adding support for data selection page

--- tangelo_html/demo/myapp.js
+++ tangelo_html/demo/myapp.js
@@ -1,8 +1,8 @@
 function runScript() {
-        $.getJSON("runNCL", function (data) {
+        $.getJSON("runNCL?selat=" + encodeURIComponent(seLat) + "?selon=" + encodeURIComponent(seLon) + "?nwlat=" + encodeURIComponent("nwLat") + "?nwlon=" + encodeURIComponent("nwLon") + "?startyear=" + encodeURIComponent(start) + "?endyear=" + encodeURIComponent(end), function (data) {
 		if(data.image)
 		{
-			localStorage.setItem("sourceimage", data.image);
+			localStorage.setItem("sourceimage?", data.image);
 			window.open("resultPage.html", "_self");
 		}
 		else
