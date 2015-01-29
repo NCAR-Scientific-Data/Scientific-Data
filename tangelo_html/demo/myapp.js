cfebcc7b7af3fd31ff18abfce691f3a2a73bbb65
Hannah <imaginationandtech@vpn46.ucar.edu>
2015-01-29 15:03:44 -0700

    .

--- tangelo_html/demo/myapp.js
+++ tangelo_html/demo/myapp.js
@@ -22,7 +22,7 @@ function calculate() {
 	var calc = $("#calc").val();
 	var interval = $("#interval").val();
 	var out = $("#outtime").val();
-	alert(calc, interval, out);
+	alert("Calc: " + calc);
 	$.getJSON("runAggregate?filename=" + encodeURIComponent(localStorage.getItem("subset")) + "&interval=" + encodeURIComponent(interval) + "&method=" + encodeURIComponent(calc) + "&outtime=" + encodeURIComponent(outtime), function (data) {
 		if(data.result)
 		{
