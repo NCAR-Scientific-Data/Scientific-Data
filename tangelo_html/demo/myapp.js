e914ab2b93c3e6e23d75c9d6506d20e08d400f6e
Hannah <imaginationandtech@econ1-158-54-dhcp.int.colorado.edu>
2015-01-28 17:37:07 -0700

    .

--- tangelo_html/demo/myapp.js
+++ tangelo_html/demo/myapp.js
@@ -8,7 +8,7 @@ function runScript() {
         $.getJSON("runNCL?swlat=" + encodeURIComponent(swLat) + "&swlon=" + encodeURIComponent(swLon) + "&nelat=" + encodeURIComponent(neLat) + "&nelon=" + encodeURIComponent(neLon) + "&startyear=" + encodeURIComponent(start) + "&endyear=" + encodeURIComponent(end), function (data) {
 		if(data.image)
 		{
-			localStorage.setItem("sourceimage?", data.image);
+			localStorage.setItem("sourceimage", data.image);
 			window.open("resultPage.html", "_self");
 		}
 		else
