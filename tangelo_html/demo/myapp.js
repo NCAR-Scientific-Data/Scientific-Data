85ce80afb9e9398734607f98cde8c35edfabdfcf
Hannah <imaginationandtech@vpn46.ucar.edu>
2015-01-29 14:36:17 -0700

    Updated html and js to work with the new python scripts

--- tangelo_html/demo/myapp.js
+++ tangelo_html/demo/myapp.js
@@ -1,14 +1,31 @@
-function runScript() {
-		var swLat = $("#swLat").val();
-		var swLon = $("#swLon").val();
-		var neLat = $("#neLat").val();
-		var neLon = $("#neLon").val();
-		var start = $("#start").val();
-		var end = $("#end").val();
-        $.getJSON("runNCL?swlat=" + encodeURIComponent(swLat) + "&swlon=" + encodeURIComponent(swLon) + "&nelat=" + encodeURIComponent(neLat) + "&nelon=" + encodeURIComponent(neLon) + "&startyear=" + encodeURIComponent(start) + "&endyear=" + encodeURIComponent(end), function (data) {
-		if(data.image)
+function subset() {
+	var swLat = $("#swLat").val();
+	var swLon = $("#swLon").val();
+	var neLat = $("#neLat").val();
+	var neLon = $("#neLon").val();
+	var start = $("#start").val();
+	var end = $("#end").val();
+    $.getJSON("runSubset?swlat=" + encodeURIComponent(swLat) + "&swlon=" + encodeURIComponent(swLon) + "&nelat=" + encodeURIComponent(neLat) + "&nelon=" + encodeURIComponent(neLon) + "&startyear=" + encodeURIComponent(start) + "&endyear=" + encodeURIComponent(end), function (data) {
+		if(data.subset)
 		{
-			localStorage.setItem("sourceimage", data.image);
+			localStorage.setItem("subset", data.subset);
+			window.open("calculate.html", "_self");
+		}
+		else
+		{
+			alert(data.alert);
+		}
+    });
+}
+
+function calculate() {
+	var calc = $("#calc").val();
+	var interval = $("#interval").val();
+	var out = $("#outtime").val();
+	$.getJSON("runAggregate?filename=" + encodeURIComponent(localStorage.getItem("subset")) + "&interval=" + encodeURIComponent(interval) + "&method=" + encodeURIComponent(calc) + "&outtime=" + encodeURIComponent(outtime), function (data) {
+		if(data.result)
+		{
+			localStorage.setItem("calculations", data.result);
 			window.open("resultPage.html", "_self");
 		}
 		else
@@ -16,4 +33,22 @@ function runScript() {
 			alert(data.alert);
 		}
         });
+}
+
+function plot()
+{
+	$.getJSON("runPlot?filename=" + encodeURIComponent(localStorage.getItem("calculations")), function (data) {
+		if(data.image)
+		{
+			document.getElementById("results").src = data.image;
+			document.getElementById("waitMessage").innerHTML = "";
+			localStorage.removeItem("calculations");
+			localStorage.removeItem("subset");
+
+		}
+		else
+		{
+			alert(data.alert);
+		}
+    });	
 }
