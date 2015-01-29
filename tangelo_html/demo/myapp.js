e6a06c4a6dcc36b2fcbb61c002a8c27287c04f48
Hannah <imaginationandtech@econ1-158-54-dhcp.int.colorado.edu>
2015-01-28 17:20:01 -0700

    Created variables for input element values

--- tangelo_html/demo/myapp.js
+++ tangelo_html/demo/myapp.js
@@ -1,4 +1,10 @@
 function runScript() {
+		var swLat = $("#swLat").val();
+		var swLon = $("#swLon").val();
+		var neLat = $("#neLat").val();
+		var neLon = $("#neLon").val();
+		var start = $("#start").val();
+		var end = $("#end").val();
         $.getJSON("runNCL?swlat=" + encodeURIComponent(swLat) + "&swlon=" + encodeURIComponent(swLon) + "&nelat=" + encodeURIComponent("neLat") + "&nelon=" + encodeURIComponent("neLon") + "&startyear=" + encodeURIComponent(start) + "&endyear=" + encodeURIComponent(end), function (data) {
 		if(data.image)
 		{
