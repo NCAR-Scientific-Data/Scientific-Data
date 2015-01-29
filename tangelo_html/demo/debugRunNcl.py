517cac68290d87b812cbe218c8b93e639f9c2ba8
Hannah Keller <hkeller@vetsscidata.ucar.edu>
2015-01-29 10:38:48 -0700

    "updated to run 3 scripts sequentially"

--- tangelo_html/demo/debugRunNcl.py
+++ tangelo_html/demo/debugRunNcl.py
@@ -1,6 +1,14 @@
-from runNCL import run
+import runAggregate
+import runSubset
+import runPlot
+
 def main():
-     run(37.5,255.0,45.0,265.0,1990,2000)
+   subset =  runSubset.run(37.5,255.0,45.0,265.0,1990,2000)
+   subsetFile = subset["subset"]
+   aggregate = runAggregate.run(subsetFile,'month', 'mean', 'start')
+   aggregateFile = aggregate["result"]
+   plot = runPlot.run(aggregateFile)
+   print plot
 
 if __name__ == "__main__":
      main()
