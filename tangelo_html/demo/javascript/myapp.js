3d6d8e11abeccbb8cbd4284352eb08053f1c253a
Hannah <imaginationandtech@gmail.com>
2015-02-12 15:41:10 -0700

    fixed typo

--- tangelo_html/demo/javascript/myapp.js
+++ tangelo_html/demo/javascript/myapp.js
@@ -83,7 +83,7 @@ $("#sim").change(function() {
 	$("syear").empty();
 	$("eyear").empty();
 
-	for(int i = start; i <= end; i++)
+	for(var i = start; i <= end; i++)
 	{
 		$("syear").append($('<option></option>').val(i).html(i.toString()));
 		$("eyear").append($('<option></option>').val(i).html(i.toString()));
