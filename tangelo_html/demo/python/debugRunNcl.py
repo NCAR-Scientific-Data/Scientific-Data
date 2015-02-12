43cfc8c4c07b40a6c98777bb7a29f6a45f9548c3
Hannah Keller <hkeller@vetsscidata.ucar.edu>
2015-02-11 17:36:29 -0700

    Permanently removed ...

--- tangelo_html/demo/python/debugRunNcl.py
+++ tangelo_html/demo/python/debugRunNcl.py
@@ -3,14 +3,14 @@ import runSubset
 import runPlot
 
 def main():
-   #subset =  runSubset.run("",37.5,255.0,45.0,265.0,1990,2000)
-   #subsetFile = subset["subset"]
-   #print subsetFile
+   subset =  runSubset.run("tmin.CRCM.ncep.dayavg.native.nc",37.5,255.0,45.0,265.0,1990,2000)
+   subsetFile = subset["subset"]
+   print subsetFile
    #aggregate = runAggregate.run(subsetFile,'month', 'mean', 'start')
    #aggregateFile = aggregate["result"]
    #print aggregateFile
-   plot = runPlot.run("../tmin_aggregate_monthly.nc",1,True)
-   print plot
+   #plot = runPlot.run("../tmin.CRCM.ncep.dayavg.native.nc",1,True)
+  # print plot
 
 if __name__ == "__main__":
      main()
