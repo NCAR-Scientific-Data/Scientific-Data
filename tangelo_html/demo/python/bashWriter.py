856e01d18b767a0fb238207b457c57f9f19b5f04
rpcrimi <Robert.Crimi@colorad.edu>
2015-02-12 19:08:33 -0700

    Fixed bashWriter

--- tangelo_html/demo/python/bashWriter.py
+++ tangelo_html/demo/python/bashWriter.py
@@ -2,6 +2,17 @@ from observable import Observable
 from observer import Observer
  
 class BashWriter(Observer):
+
+    def getCurrentStep(self, filename):
+        stepString = "# CURRENT STEP:"
+        f = open(filename, 'r')
+        lines = f.readlines()
+        stepLine = lines[1].split()
+        for s in stepLine:
+            if s.isdigit():
+                num = int(s)
+        return num
+
     def update(self, *args, **kwargs):
         dest = args[0]
         if(dest == 'bashWriter'):   
@@ -12,13 +23,18 @@ class BashWriter(Observer):
                 message = args[3]
                 comment = args[4]
                 filename = args[5]
+                
             elif(task == 'append'):
                 message = args[2]
                 comment = args[3]
                 filename = args[4]
-            elif(task == 'move'):
+            elif(task == 'moveStep'):
                 index = args[2]
                 filename = args[3]
+            elif(task == 'delSteps'):
+                i = args[2]
+                j = args[3]
+                filename = args[4]
 
             # Run Task
             if(task == 'append'):
@@ -48,12 +64,33 @@ class BashWriter(Observer):
                     for line in lines:
                         f.write(line)
                     f.close()
-            elif(task == 'move'):
+            elif(task == 'moveStep'):
                 f = open(filename, 'r')
                 lines = f.readlines()
-                currentIndex = lines.index("# CURRENT STEP\n")
-                lines[index], lines[currentIndex] = lines[currentIndex], lines[index]
+                stepLine = lines[1].split()
+                newLine = ''
+                for s in stepLine:
+                    if s.isdigit():
+                        newLine += str(index)
+                    else:
+                        newLine += (s + " ")
+                newLine += '\n'
                 f.close()
+                f = open(filename, 'w')
+                for line in lines:
+                    if("CURRENT STEP:" in line):
+                        f.write(newLine)
+                    else:
+                        f.write(line)
+                f.close()
+            elif(task == 'delSteps'):
+                f = open(filename, 'r+')
+                lines = f.readlines()
+                if(j <= len(lines)):
+                    for x in range(i,j):
+                        del lines[x]
+                    f.close()
+
                 f = open(filename, 'w')
                 for line in lines:
                     f.write(line)
@@ -70,5 +107,5 @@ if __name__ == "__main__":
     observable.register(bashWriter)
  
     #observable.update_observers('bashWriter',  'insert', 1, "free willy \n", 'bash/test.sh')
-    observable.update_observers('foo', 'append', 0, "echo hello mars", 'bash/test.sh')
-    observable.update_observers('bashWriter', 'move', 2, 'bash/test.sh')
+    #observable.update_observers('bashWriter', 'append', 0, "echo hello mars", 'bash/test.sh')
+    observable.update_observers('bashWriter', 'moveStep', 3, 'bash/test.sh')
