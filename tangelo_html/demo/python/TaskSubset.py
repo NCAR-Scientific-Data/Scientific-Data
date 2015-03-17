43312796e5a191cb46c13ad124d54f29278b4028
hannahkeller <keller.hannah6@gmail.com>
2015-03-17 17:39:21 -0600

    renamed python tasks and added unit conversion task

---
+++ tangelo_html/demo/python/TaskSubset.py
@@ -0,0 +1,53 @@
+"""@package docstring
+This script subsets a NetCDF file via a NCL script. The NCL script subsets the NetCDF file by
+a bounding rectangle given a Southwest Lat/Lon point and Northeast Lat/Lon point. It then creates
+a new NetCDF file of the subsetted data
+"""
+import subprocess
+import sys
+import re
+import pyutilib.workflow
+
+class TaskSubset(pyutilib.workflow.Task):
+    def __init__(self,*args,**kwds):
+        """Constructor."""
+        pyutilib.workflow.Task.__init__(self,*args,**kwds)
+        self.inputs.declare('url')
+        self.inputs.declare('variable')
+        self.inputs.declare('swlat')
+        self.inputs.declare('swlon')
+        self.inputs.declare('nelat')
+        self.inputs.declare('nelon')
+        self.inputs.declare('startdate')
+        self.inputs.declare('enddate')
+        self.outputs.declare('subset')
+
+    def execute(self):
+        swLat = "swLat={0}".format(self.swlat)
+        swLon = "swLon={0}".format(self.swlon)
+        neLat = "neLat={0}".format(self.nelat)
+        neLon = "neLon={0}".format(self.nelon)
+        startDate = "startDate={0}".format(self.startdate)
+        endDate = "endDate={0}".format(self.enddate)
+        filename = "filename=\"{0}\"".format(self.url)
+        v = "variable=\"{0}\"".format(self.variable)
+        
+        args = ['ncl', '-n', filename, v, swLat, swLon, neLat, neLon, startDate, endDate, 'ncl/narccap_subset_tmin_time_latlon.ncl']
+        sysError = False
+        nclError = False
+        try:
+            status = subprocess.Popen(args,stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        except:
+            sysError = True
+            error = "System error, please contact the site administrator"
+
+        if not sysError:
+            for line in status.stdout:
+                if line.find("fatal") != -1:
+                    nclError = True
+                    error = re.sub('\[.*?\]:',' ',line)
+                    break
+        if nclError or sysError:
+            self.subset = { "error": error }
+        else:
+            self.subset = { "subset": "tmin_subset_time_latlon.nc" }
