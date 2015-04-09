66b175f250c0c5dbfa87ff9f94d7fb2b922f28ef
Hannah <imaginationandtech@gmail.com>
2015-04-09 07:37:51 -0600

    Removed useless .ready() and replaced "use strict"; strings.

--- tangelo_html/ncarworkflow/javascript/minimenu.js
+++ tangelo_html/ncarworkflow/javascript/minimenu.js
@@ -1,29 +1,25 @@
-$( document ).ready(function() {
-  		
-});
-
 function toggleMiniMenu() {
 	"use strict";
 	$(".miniMenuOption").toggle("fast");
 }
 
 function newSubset() {
+	"use strict";
   	$("#StepMaker").empty();
 	$("#StepMaker").load("Subset.html")
-	console.log("Stepmaker:", $("#StepMaker"));
-	
+	//console.log("Stepmaker:", $("#StepMaker"));
 }
 
 function newAnalysis() {
+	"use strict";
 	$("#StepMaker").empty();
 	$("#StepMaker").load("Analysis.html")
 }
 
 function newPlot() {
-
+	"use strict";
 }
 
 function newDownload() {
-
+	"use strict";
 }
-
