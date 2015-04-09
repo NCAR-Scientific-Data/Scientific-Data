6c05d04f6a84b11729d7b3d26232dd3a409334eb
boyuhan <boha4482@colorado.edu>
2015-04-09 16:15:56 -0600

    Create output directory and filename with its workflowID and uid

--- tangelo_html/ncarworkflow/python/TaskDelta.py
+++ tangelo_html/ncarworkflow/python/TaskDelta.py
@@ -13,14 +13,26 @@ class taskDelta(pyutilib.workflow.Task):
 
     def execute(self):
 	
-	#Create a output file by copyinh the first file since
-	#    R is inefficient when output to new NetCDF file
-	command = "cp " + self.filename1 + " " + self.outputfname
-	os.system(command)
+	# Check if workflow directory exists, if not create one
+	wid = self.workflowID
+	tid = self.id
+	
+	workflowDirName = "/data/" + wid + "/" 
+	if not os.path.isdir(workflowDirName): os.system("mkdir " + workflowDirName)
+	
+	# Uniquely name output file by task id
+	outputFname = workflowDirName + tid + "_delta.nc"
+	if os.path.exists(outputFname): os.system("rm -rf " + outputFname)
 
 	# Calculate delta using R script
-	scriptName = "ncarworkflow/r/calculationModule.R"
+	scriptName = "/home/project/Scientiic-Data/tangelo_html/ncarworkflow/r/calculationModule.R"
 	ro.r['source'](scriptName)
 
+	# Check if workflow directory exists, if not create one
+	#Create a output file by copyinh the first file since
+	#    R is inefficient when output to new NetCDF file
+	command = "cp " + self.filename1 + " " + outputFname
+	os.system(command)
+
 	# Call the function that does the calculation
-	ro.r['ncdfDelta'](self.filename1, self.filename2, self.outputfname, self.field)
+	ro.r['ncdfDelta'](self.filename1, self.filename2, outputFname, self.field)
