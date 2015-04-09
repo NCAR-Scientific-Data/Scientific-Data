0fae23193a8c8c66561ab214a41c7187660af9fb
Seongmin Choi <sungpa1919@gmail.com>
2015-04-09 00:17:06 -0600

    updated minimenu

--- tangelo_html/ncarworkflow/javascript/minimenu.js
+++ tangelo_html/ncarworkflow/javascript/minimenu.js
@@ -1,22 +1,29 @@
-/*global $*/
+$( document ).ready(function() {
+  		
+});
 
 function toggleMiniMenu() {
-    "use strict";
-    $(".miniMenuOption").toggle("fast");
+	"use strict";
+	$(".miniMenuOption").toggle("fast");
 }
 
 function newSubset() {
-    "use strict";
+  	$("#StepMaker").empty();
+	$("#StepMaker").load("Subset.html")
+	console.log("Stepmaker:", $("#StepMaker"));
+	
 }
 
 function newAnalysis() {
-    "use strict";
+	$("#StepMaker").empty();
+	$("#StepMaker").load("Analysis.html")
 }
 
 function newPlot() {
-    "use strict";
+
 }
 
 function newDownload() {
-    "use strict";
+
 }
+
