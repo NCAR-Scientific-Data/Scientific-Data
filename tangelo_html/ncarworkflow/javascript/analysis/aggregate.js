b6c02de2fb0742ad8c20ef24b631bbbf0811d1ee
hannahkeller <keller.hannah6@gmail.com>
2015-04-22 18:55:01 -0600

    added html and js for aggregate task

--- tangelo_html/ncarworkflow/javascript/analysis/aggregate.js
+++ tangelo_html/ncarworkflow/javascript/analysis/aggregate.js
@@ -1,32 +1,57 @@
 /*global localStorage, $*/
 
-function aggregate(calc, interval, out) {
+function aggregate(filename, calc, interval, out, cyclic, repopulateVals) {
     "use strict";
 
-    calc = "&method=" + calc;
-    interval = "&interval=" + interval;
-    out = "&outtime=" + out;
-    var url = "python/runCalculation?filename=" + encodeURIComponent(localStorage.getItem("subset")) + interval + calc + out;
+    var inputs = {
+        "filename" : filename,
+        "calc" : calculation,
+        "interval" : interval,
+        "out" : out,
+        "cyclic" : cyclic
+    };
 
-    $("<p>Running Calculations. Please Wait.</p>").insertAfter($(".form-inline"));
-
-    $.getJSON(url, function (data) {
-        if (data.result) {
-            localStorage.result = data.result;
-            $("p").html("Calculations Succesful!");
-        } else {
-            localStorage.result = "";
-            $("p").html("Calculations Failed.<br>" + data.error);
-        }
-    });
+    addTask("taskAggregate", inputs, repopulateVals, "aggregate");
 }
 
+//Parse values from form
 function callAggregate() {
     "use strict";
 
-    var calc = encodeURIComponent($("#calc option:selected").val()),
-        interval = encodeURIComponent($("#interval option:selected").val()),
-        out = encodeURIComponent($("#outtime option:selected").val());
+    var allNodes = JSON.parse(localStorage.nodes),
+        selectedNode = $("#node option:selected").val(),
+        filename = ["Port", selectedNode, allNodes[selectedNode].output],
+        calculation = $("#calc option:selected").val(),
+        interval = $("#interval option:selected").val(),
+        out = $("#outtime option:selected").val(),
+        cyclic = $("input[name='cyclic']:checked").val();
 
-    aggregate(calc, interval, out);
+    var repopulateVals = {
+        "html" : "stepHTML/aggregate.html",
+        "values" : {
+            "#calc" : calculation,
+            "#node" : filename[1],
+            "#interval" : interval,
+            "#outtime" : out
+        }
+    };
+
+    var cyclicSelector = "input[name='" + cyclic + "']";
+
+    repopulateVals.values[cyclicSelector] = true;
+    aggregate(filename, calc, interval, out, repopulateVals);
+}
+
+function generateNodeSelect(){
+    "use strict";
+    var nodeDropDown = $("#node");
+
+    var nodes = JSON.parse(localStorage.nodes);
+
+    for(var node in nodes){
+        if (node != "workflowID"){
+            var n = nodes[node];
+            nodeDropDown.append($("<option></option>").val(node).html(n.name));
+        }
+    }
 }
