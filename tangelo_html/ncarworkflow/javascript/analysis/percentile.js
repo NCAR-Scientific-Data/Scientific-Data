6a5a0b9ccde34fbd96563f87f07c0f61ea667e6b
boyuhan <boha4482@colorado.edu>
2015-04-29 13:46:16 -0600

    Added update function call to percentile

--- tangelo_html/ncarworkflow/javascript/analysis/percentile.js
+++ tangelo_html/ncarworkflow/javascript/analysis/percentile.js
@@ -30,6 +30,30 @@ function callPercentile() {
     percentile(filename, percentage, repopulateVals);
 }
 
+function updatePercentile() {
+    "use strict";
+
+    var allNodes = JSON.parse(localStorage.nodes),
+        selectedNode = $("#node option:selected").val(),
+        filename = ["Port", selectedNode, allNodes[selectedNode].output],
+        percentage = $("#percentage").val();
+
+    var repopulateVals = {
+        "html" : "stepHTML/percentile.html",
+        "values" : {
+            "#percentage" : percentage,
+            "#node"  : selectedNode
+        }
+    };
+
+    var inputs = {
+        "filename" : filename,
+        "percentage" : percentage
+    };
+
+    updateTask(inputs, repopulateVals);
+}
+
 function generateNodeSelect() {
     "use strict";
     var nodeDropDown = $("#node");
