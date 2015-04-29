ef7179a8d1742f0a9771f4b817ab44079695fadc
Hannah <imaginationandtech@gmail.com>
2015-04-29 00:11:22 -0600

    Removed junk folder

--- junk/test_scripts/popen-test.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-import sys
-
-def run():
-    args = ['ncl', 'filename=\"tmin_aggregate_monthly.nc\"', '../tangelo_html/demo/ncl/narccap_plot_tmin_native.ncl']
-    p = subprocess.Popen(args)
-
-if __name__ == "__main__":
-    run()
