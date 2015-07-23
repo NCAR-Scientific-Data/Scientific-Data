c3d3c1eb63a9a979e7eb6ee71366f73f33a37848
Robert Crimi <rpcrimi@gmail.com>
2015-07-23 11:59:57 -0600

    fixed time bounds

--- tangelo_html/ncarworkflow/plugin/workflow/python/customTasks/PluginTaskSubset.py
+++ tangelo_html/ncarworkflow/plugin/workflow/python/customTasks/PluginTaskSubset.py
@@ -70,7 +70,7 @@ class PluginTaskSubset(pyutilib.workflow.TaskPlugin):
         wid = "wid=\"{0}\"".format(self.workflowID)
         tid = "tid=\"{0}\"".format(self.uid)
 
-        args = ['/usr/local/ncl/bin/ncl', '-Q', filename, v, swLat, swLon, neLat, neLon, startDate, endDate, wid, tid, "../plugin/workflow/python/customTasks/ncl/subset_time_latlon.ncl"]
+        args = ['ncl', '-Q', filename, v, swLat, swLon, neLat, neLon, startDate, endDate, wid, tid, "../plugin/workflow/python/customTasks/ncl/subset_time_latlon.ncl"]
         args = filter(None,args)
         sysError = False
         nclError = False
